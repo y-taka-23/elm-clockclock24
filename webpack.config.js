f4e5c47de56e8f962f80975b41de7726ae1ec421
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2018-09-17 03:41:08 +0900

    Setup webpack and the dev-server

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: [
+            path.resolve(__dirname, 'src', 'index.js'),
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'docs'),
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                loader: 'file-loader?name=[name].[ext]',
+            },
+            {
+                test: /\.elm$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                loader: 'elm-webpack-loader?verbose=true',
+            },
+        ],
+        noParse: /\.elm$/
+    },
+    devServer: {
+        inline: true,
+        stats: { colors: true },
+    }
+};
