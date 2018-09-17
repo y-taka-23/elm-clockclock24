6848476e7f338bab141aa6b22ad2c6df32ed5d5e
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2018-09-18 05:14:44 +0900

    Define digital segments as 6 clocks

--- src/Model.elm
+++ src/Model.elm
@@ -1,14 +1,212 @@
 module Model exposing (Model, Msg(..), init)
 
+import List.Extra as List
+
 
 type Msg
     = NoOp
 
 
 type alias Model =
-    Int
+    { displayed : Display
+    }
 
 
 init : Model
 init =
-    0
+    { displayed = noDigits
+    }
+
+
+type alias Clock =
+    { hour : Float
+    , minute : Float
+    }
+
+
+blank : Clock
+blank =
+    { hour = 225
+    , minute = 225
+    }
+
+
+vertical : Clock
+vertical =
+    { hour = 180
+    , minute = 0
+    }
+
+
+down : Clock
+down =
+    { hour = 180
+    , minute = 180
+    }
+
+
+up : Clock
+up =
+    { hour = 0
+    , minute = 0
+    }
+
+
+right : Clock
+right =
+    { hour = 90
+    , minute = 90
+    }
+
+
+left : Clock
+left =
+    { hour = 270
+    , minute = 270
+    }
+
+
+downRight : Clock
+downRight =
+    { hour = 180
+    , minute = 90
+    }
+
+
+upRight : Clock
+upRight =
+    { hour = 0
+    , minute = 90
+    }
+
+
+downLeft : Clock
+downLeft =
+    { hour = 180
+    , minute = 270
+    }
+
+
+upLeft : Clock
+upLeft =
+    { hour = 0
+    , minute = 270
+    }
+
+
+type alias Digit =
+    { topLeft : Clock
+    , middleLeft : Clock
+    , bottomLeft : Clock
+    , topRight : Clock
+    , middleRight : Clock
+    , bottomRight : Clock
+    }
+
+
+digits : List Digit
+digits =
+    [ { topLeft = downRight
+      , middleLeft = vertical
+      , bottomLeft = upRight
+      , topRight = downLeft
+      , middleRight = vertical
+      , bottomRight = upLeft
+      }
+    , { topLeft = blank
+      , middleLeft = blank
+      , bottomLeft = blank
+      , topRight = down
+      , middleRight = vertical
+      , bottomRight = up
+      }
+    , { topLeft = right
+      , middleLeft = downRight
+      , bottomLeft = upRight
+      , topRight = downLeft
+      , middleRight = upLeft
+      , bottomRight = left
+      }
+    , { topLeft = right
+      , middleLeft = right
+      , bottomLeft = right
+      , topRight = downLeft
+      , middleRight = upLeft
+      , bottomRight = upLeft
+      }
+    , { topLeft = down
+      , middleLeft = upRight
+      , bottomLeft = blank
+      , topRight = down
+      , middleRight = vertical
+      , bottomRight = up
+      }
+    , { topLeft = downRight
+      , middleLeft = upRight
+      , bottomLeft = right
+      , topRight = left
+      , middleRight = downLeft
+      , bottomRight = upLeft
+      }
+    , { topLeft = downRight
+      , middleLeft = vertical
+      , bottomLeft = upRight
+      , topRight = left
+      , middleRight = downLeft
+      , bottomRight = upLeft
+      }
+    , { topLeft = right
+      , middleLeft = blank
+      , bottomLeft = blank
+      , topRight = downLeft
+      , middleRight = vertical
+      , bottomRight = up
+      }
+    , { topLeft = downRight
+      , middleLeft = upRight
+      , bottomLeft = upRight
+      , topRight = downLeft
+      , middleRight = upLeft
+      , bottomRight = upLeft
+      }
+    , { topLeft = downRight
+      , middleLeft = upRight
+      , bottomLeft = right
+      , topRight = downLeft
+      , middleRight = vertical
+      , bottomRight = upLeft
+      }
+    ]
+
+
+unknown : Digit
+unknown =
+    { topLeft = blank
+    , middleLeft = blank
+    , bottomLeft = blank
+    , topRight = blank
+    , middleRight = blank
+    , bottomRight = blank
+    }
+
+
+toDigit : Int -> Digit
+toDigit n =
+    Maybe.withDefault unknown <| List.getAt n digits
+
+
+type alias Display =
+    { d1 : Digit
+    , d2 : Digit
+    , d3 : Digit
+    , d4 : Digit
+    }
+
+
+noDigits : Display
+noDigits =
+    { d1 = unknown
+    , d2 = unknown
+    , d3 = unknown
+    , d4 = unknown
+    }
